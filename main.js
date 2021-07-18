722a57793373e01e745875dd386092da83414e9a
JYeonDev <whddus4837@gmail.com>
2021-07-18 20:06:02 +0900

    OMDb API

--- main.js
+++ main.js
@@ -5,9 +5,36 @@ import getType from './getType'
 // ex) user: heropy import에서는 :(콜론)대신 as 를 통해 바꿀수 있다.
 import * as R from './getRandom'
 // import 안에 내용을 한번에 가져올때 사용 *(와일드카드)
+import myData from './myData.json'
+import axios from 'axios'
 
-console.log(getType(5))
-// console.log(random())
-//console.log(heropy);
-console.log(R)
+/* console.log(myData);
 
+const usersA = [
+  { userId: '1', name: 'HEROPY'},
+  { userId: '2', name: 'NEO'}
+]
+
+const usersB = [
+  { userId: '1', name: 'HEROPY'},
+  { userId: '3', name: 'AMY'}
+]
+const usersC = usersA.concat(usersB)
+console.log('concat', usersC)
+console.log('uniqBy', _.uniqBy(usersC, 'userId'))
+
+const usersD = _.unionBy(usersA, usersB, 'userId')
+console.log('unionBy', usersD) */
+
+function fetchMovies() {
+  axios
+  .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+  .then(res => {
+    console.log(res)
+    const h1El = document.querySelector('h1')
+    const imgEl = document.querySelector('img')
+    h1El.textContent = res.data.Search[0].Title
+    imgEl.src = res.data.Search[0].Poster
+  })
+}
+fetchMovies()
